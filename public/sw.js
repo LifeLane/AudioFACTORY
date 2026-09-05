d2838614ec27a4721251763e827ee0757c2e2c78
ProfessorAI <53007606+LifeLane@users.noreply.github.com>
2026-09-06 03:13:25 +0530

    DNS updated

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11734980
+    "domain": "3nbf4.com",
+    "zoneId": 11734995
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
